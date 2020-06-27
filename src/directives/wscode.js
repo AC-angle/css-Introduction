4ee9669f1ec979ca5d228ea07a37ea6dd785e043
心叶 <yelloxing@gmail.com>
2020-06-27 17:09:21 +0800

    修改居中左对齐inline元素中错误的宽度

--- src/directives/wscode.js
+++ src/directives/wscode.js
@@ -16,9 +16,20 @@ export default {
 
         new WSCode({
             el,
-            content: el.innerText.replace(/\n{2}/g,'\n'),
+            content: el.innerText.replace(/\n{2}/g, '\n'),
             lang: {
-                type: binding.type
+                type: binding.type,
+
+                // 配置更细节的着色（可选）
+                color: {
+                    "annotation": "#6a9955",/*注释颜色*/
+                    "border": "#000000",/*边界颜色*/
+                    "tag": "#1e50b3",/*结点颜色*/
+                    "attr": "#1e83b1",/*属性颜色*/
+                    "string": "#ac4c1e",/*字符串颜色*/
+                    "key": "#ff0000",/*关键字颜色*/
+                }
+
             }
         });
 
